77cb1111f55897ea99504f784d9808368d7b7c7d
Artem Tsykman <dofpool@gmail.com>
2023-10-06 00:49:33 +0300

    no message

---
+++ src/main/Java/by/lesson/AM_1/Main_and_MathOperation.java
@@ -0,0 +1,12 @@
+package by.lesson.AM_1;
+
+public class Main_and_MathOperation {
+    public static void main(String[] args) {
+        double num1 = 3.45;
+        double num2 = 5.5;
+        double num3 = 7.78;
+        var result = num1 * num2 * num3;
+        System.out.println ("Результат = " + result);
+        }
+    }
+
