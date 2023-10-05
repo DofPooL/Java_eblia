6c2404daad4ab1e46b732628101d09f2ea13a236
Artem Tsykman <dofpool@gmail.com>
2023-10-05 21:43:08 +0300

    реализован метод сложения двух чисел

---
+++ src/main/java/by/lesson/task1/SumNumbers.java
@@ -0,0 +1,12 @@
+package by.lesson.task1;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        int numberOne = 2;
+        int numberTwo = 4;
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("sum = " + sum);
+    }
+}
