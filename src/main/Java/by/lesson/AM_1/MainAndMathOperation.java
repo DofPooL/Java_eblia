73b00230207d30a6dec9817415eb03ee5df10ddf
Artem Tsykman <dofpool@gmail.com>
2023-10-11 00:53:34 +0300

    Убрал 18 строку

--- src/main/Java/by/lesson/AM_1/MainAndMathOperation.java
+++ src/main/Java/by/lesson/AM_1/MainAndMathOperation.java
@@ -2,9 +2,8 @@ package by.lesson.AM_1;
 
 import java.util.Scanner;
 
-public class Main_and_MathOperation {
-public static void main(String[]args)
-        {
+public class MainAndMathOperation {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число 1: ");
         double num1 = in.nextDouble();
@@ -13,6 +12,6 @@ public static void main(String[]args)
         System.out.print("Введите число 3: ");
         double num3 = in.nextDouble();
         double result = num1 * num2 * num3;
-        System.out.println("Результат = "+result);
-        }
-        }
+        System.out.println("Результат = " + result);
+    }
+}
