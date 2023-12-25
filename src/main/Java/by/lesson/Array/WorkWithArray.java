736d5672e05e62a10c9f32b170e50dc2eeb6f2b6
Artem Tsykman <dofpool@gmail.com>
2023-12-25 23:08:02 +0300

    amended two commits

---
+++ src/main/Java/by/lesson/Array/WorkWithArray.java
@@ -0,0 +1,48 @@
+package by.lesson.Array;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class WorkWithArray {
+
+    public static void main(String[] args) {
+        //Сортировки, сортировка пузырьком, что дает static
+        WorkWithArray workWithArray = new WorkWithArray();
+        int[] arrayNumbers = workWithArray.generatorRandomNumbers(100);
+        System.out.println(Arrays.toString(arrayNumbers));
+        System.out.println(workWithArray.maxNumber(arrayNumbers));
+        System.out.println(workWithArray.minNumber(arrayNumbers));
+        System.out.println(Arrays.toString(workWithArray.findMaxNumbers(arrayNumbers)));
+    }
+
+    private int maxNumber(int[] numberArray) {
+        int max = numberArray[0];
+        for (int i = 0; i < numberArray.length; i++) {
+            if (max < numberArray[i]) {
+                max = numberArray[i];
+            }
+        }
+        return max;
+    }
+
+    private int minNumber(int[] numberArray) {
+        int min = numberArray[0];
+        for (int i = 0; i < numberArray.length; i++) {
+            if (min > numberArray[i]) {
+                min = numberArray[i];
+            }
+        }
+        return min;
+    }
+
+    private int[] generatorRandomNumbers(int sizeArray) {
+        int[] numberArray = new int[sizeArray];
+        Random random = new Random();
+        for (int i = 0; i < sizeArray; i++) {
+            numberArray[i] = random.nextInt(100);
+        }
+        return numberArray;
+    }
+    
+
+}
