ae943bb799d70d36eb9170ed11ef87ede841eab0
Artem Tsykman <dofpool@gmail.com>
2023-12-25 23:08:02 +0300

    amended two commits Два коммита в 1

---
+++ src/main/Java/by/lesson/PersonsArray/ArrayPersons.java
@@ -0,0 +1,10 @@
+package by.lesson.PersonsArray;
+
+import by.lesson.StackLessons.Person;
+
+public class ArrayPersons {
+    public static void main(String[] args) {
+        Person[] personsArray = new Person[10];
+
+    }
+}
